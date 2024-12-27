aac266604a26a7e9fc5c06eb40757d19c5641df4
Stepan Zhizhin <stepan.zhizhin@mail.ru>
2024-12-27 23:34:53 +0500

    Модуль 11.1

--- js/picture.js
+++ js/picture.js
@@ -1,28 +1,27 @@
-import { getPictures } from './data.js';
 import {showBigPicture} from './big-picture.js';
 
-const list = document.querySelector('.pictures');
+const renderPictures = (pictures) => {
+  const list = document.querySelector('.pictures');
 
-const picTemplate = document.querySelector('#picture').content.querySelector('a');
-const pictures = getPictures();
-const fragment = document.createDocumentFragment();
+  const picTemplate = document.querySelector('#picture').content.querySelector('a');
+  const fragment = document.createDocumentFragment();
 
-pictures.forEach((picture) => {
-  const element = picTemplate.cloneNode(true);
+  pictures.forEach((picture) => {
+    const element = picTemplate.cloneNode(true);
 
-  element.children[0].src = picture.url;
-  element.children[0].alt = picture.descriptions;
-  element.querySelector('.picture__comments').textContent = picture.comments.length;
-  element.querySelector('.picture__likes').textContent = picture.likes;
+    element.children[0].src = picture.url;
+    element.children[0].alt = picture.descriptions;
+    element.querySelector('.picture__comments').textContent = picture.comments.length;
+    element.querySelector('.picture__likes').textContent = picture.likes;
 
-  element.addEventListener('click', () => {
-    showBigPicture(picture);
+    element.addEventListener('click', () => {
+      showBigPicture(picture);
+    });
+
+    fragment.appendChild(element);
   });
 
-  fragment.appendChild(element);
+  list.appendChild(fragment);
+};
 
-});
-
-list.appendChild(fragment);
-
-export {pictures};
+export {renderPictures};
